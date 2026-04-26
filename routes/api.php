61871e14b90ded07db5ad83c6b4bdba5fde0051e
NileshPMore <151550560+NileshPMore@users.noreply.github.com>
2026-04-26 22:39:16 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,6 @@ use Illuminate\Support\Facades\Route;
 
 //Here, I use rate limiting to allow only 10 requests per minute.
 
-Route::post('/scanapidata',[App\Http\Controllers\API\ScanController::class,'payloadapidata'])->middleware('throttle:scanapidata');
+Route::post('/scanapidata',[App\Http\Controllers\API\ScanController::class,'scanapidata'])->middleware('throttle:scanapidata');
 
 ?>
